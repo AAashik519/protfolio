42113a75a6cc324ef112555e1aaf00c3c0c17d6b
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-08-02 03:24:38 +0600

    service section complete

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,6 +5,7 @@ import HeartEmoji from '../../img/heartemoji.png'
 import  glasses from '../../img/glasses.png'
 import humble from '../../img/humble.png'
 import Card from '../Card/Card'
+import Resume from './ashik.pdf'
 const Services = () => {
   return (
     <div  className='services'>
@@ -14,19 +15,43 @@ const Services = () => {
             <span>Services </span>
             <span>Lorem ipsum is simpley dummey text of printing of printing lorem <br></br>  ispum is simply dummey text of printing commodi.      </span>
 
-            <button className='button s-button'>Download Cv</button>
+                <a href={Resume} download >
+                <button className='button s-button'>Download Cv</button>
+
+                </a>
             <div className="blur s-blur1" style={{background :"ABF1FF94"}}></div>
 
          </div>
          {/* right side */}
          <div className="cards">
-                <div>
+                <div style={{left:'14rem'}}>
                     <Card 
                     emoji={HeartEmoji}
                     heading ={'Design'}
-                     detail={"Figma, Sketch, PhotoShop, Adobe"}
+                     detail={"Figma, Sketch, PhotoShop, Adobe Xd"}
                      />
                 </div>
+
+
+
+                <div style={{ top:'12rem' ,left:'-4rem'}}>
+                    <Card 
+                    emoji={glasses}
+                    heading ={'Developers'}
+                     detail={"React,Nodejs, Expressjs,MongoDB"}
+                     />
+                </div>
+
+
+                <div style={{ top:'19rem' ,left:'12rem'}}>
+                    <Card 
+                    emoji={humble}
+                    heading ={'Ui / UX'}
+                     detail={"Lorem ipsum is simpley dummey"}
+                     />
+                </div>
+
+                <div className='blur  s-blur2 '  style={{background:"var(--purple)"}}></div>
          </div>
 
     </div>
